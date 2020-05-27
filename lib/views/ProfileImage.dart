93145469bfd7b43c506b51da9be2f42c7828a385
Ryan Fogarty <ryan.fogarty@brighthr.com>
2020-05-27 14:58:45 +0100

    chached image

--- lib/views/ProfileImage.dart
+++ lib/views/ProfileImage.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -35,10 +36,12 @@ class _ProfileImageState extends State<ProfileImage> {
   Widget _image() {
     return ClipRRect(
         borderRadius: BorderRadius.circular(widget.circular ? 10000 : 0),
-        child: Image.network(
-            widget.imageUrl ??
+        child: CachedNetworkImage(
+            imageUrl: widget.imageUrl ??
                 "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
             filterQuality: FilterQuality.high,
-            height: widget.height));
+            height: widget.height,
+            placeholder: (context, url) =>
+                Center(child: CircularProgressIndicator())));
   }
 }
