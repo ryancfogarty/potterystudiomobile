22c22df00421d4f837c7011b1d10011679935e69
Ryan Fogarty <rfogar2@uic.edu>
2020-08-01 23:26:37 +0100

    fix first login bug

--- lib/usecases/CreateStudioUseCase.dart
+++ lib/usecases/CreateStudioUseCase.dart
@@ -1,13 +1,17 @@
 import 'package:pottery_studio/repositories/StudioRepository.dart';
 import 'package:pottery_studio/services/AuthService.dart';
 
+import 'GetUserUseCase.dart';
+
 class CreateStudioUseCase {
   StudioRepository _repo;
   AuthService _authService;
+  GetUserUseCase _getUserUseCase;
 
-  CreateStudioUseCase(StudioRepository repo, AuthService authService) {
+  CreateStudioUseCase(StudioRepository repo, AuthService authService, GetUserUseCase getUserUseCase) {
     _repo = repo;
     _authService = authService;
+    _getUserUseCase = getUserUseCase;
   }
 
   Future<void> createStudio(
@@ -15,6 +19,7 @@ class CreateStudioUseCase {
     var success = await _repo.createStudio(userName, studioName, imageUrl);
 
     if (success) {
+      await _getUserUseCase.getUser(clearCache: true);
       _authService.updateState(AppState.REGISTERED);
     } else {
       throw "studio created but user not created";
